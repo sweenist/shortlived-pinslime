09683a8825858ed524ab2cc160d03a83a3efe944
Ryan Sweeney <sween119@hotmail.com>
2025-10-21 09:34:26 -0400

    bring back dialog events for now

--- src/events/eventConstants.ts
+++ src/events/eventConstants.ts
@@ -2,8 +2,11 @@ export const signals = {
   heroPosition: 'HERO_POSITION',
   heroItemCollect: 'HERO_COLLECTS_ITEM',
   heroInteraction: 'HERO_INTERACTION',
+  heroContact: 'HERO_CONTACT',
   sceneExit: 'SCENE_EXIT',
   levelChanging: 'LEVEL_CHANGING',
   levelChanged: 'LEVEL_CHANGED',
   stateChanged: 'GAME_STATE_CHANGE',
+  startTextInteraction: 'START_TEXT_INTERACTION',
+  endTextInteraction: 'END_TEXT_INTERACTION',
 };
