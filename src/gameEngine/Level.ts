7e98445c638d88d26add614ce66a3c6cee8d8993
Ryan Sweeney <sween119@hotmail.com>
2025-10-25 13:10:58 -0400

    clean up unused interface and function

--- src/gameEngine/Level.ts
+++ src/gameEngine/Level.ts
@@ -2,19 +2,10 @@ import { GameObject } from './GameObject';
 import type { Sprite } from './Sprite';
 import { gameEvents } from '../events/Events';
 import { type Vector2Interface, Vector2 } from '../utils/vector';
-import Obstacle from '../objects/Obstacles/Obstacle';
-import { resources } from '../Resources';
 
 export interface LevelConfig {
   walls: { x: number; y: number }[];
   obstacles?: {};
-}
-
-export interface GameObjectParams {
-  frameIndex: number;
-  imageName: string;
-  isSolid?: boolean;
-  positions: Vector2Interface[];
 }
 
 export interface FrameConfig {
@@ -24,6 +15,7 @@ export interface FrameConfig {
 }
 export interface ResourceConfig {
   name: string;
+  startIndex?: number;
   frameConfig: FrameConfig;
 }
 
@@ -53,29 +45,6 @@ export class Level extends GameObject {
   defineWalls(config?: LevelConfig) {
     config?.walls.forEach(({ x, y }) => {
       this.walls.add(`x: ${x}, y: ${y}`);
-    });
-  }
-
-  layoutObstacles(config: ResourceConfig[], params: GameObjectParams) {
-    const res = config.find((r) => r.name === params.imageName);
-    if (!res) {
-      throw `Cannot find ${params.imageName} in resources`;
-    }
-    const { frameConfig } = res;
-    params.positions.forEach((position) => {
-      const sprite = new Obstacle({
-        isSolid: params.isSolid,
-        position: Vector2.fromPoint(position),
-        content: {
-          resource: resources.images[params.imageName],
-          frameColumns: frameConfig.columns,
-          frameRows: frameConfig.rows,
-          frameSize: Vector2.fromPoint(frameConfig.size),
-          frameIndex: params.frameIndex,
-        },
-      });
-
-      this.addChild(sprite);
     });
   }
 }
